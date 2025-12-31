19fa4d28ac6df78801a61a058376db6a5193599e
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2025-12-31 11:41:20 +0200

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -89,7 +89,7 @@ resource "aws_sns_topic" "hhc" {
   tags = merge(var.billing_tags, {
     git_org   = "SEA"
     git_repo  = "cloud-analytics"
-    yor_trace = "954e452d-8731-439c-9524-780a932f07cb"
+    yor_trace = "954e452d-8731-439c-9524-780a932f07aa"
   })
   kms_master_key_id = "alias/aws/sns"
 }
