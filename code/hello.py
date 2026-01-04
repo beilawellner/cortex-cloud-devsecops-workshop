dd2c7c6594e76bb6446f37a4c5e144147e3a3b1f
Beila Wellner <44134979+beilawellner@users.noreply.github.com>
2026-01-04 11:53:23 +0200

    Update hello.py

--- code/hello.py
+++ code/hello.py
@@ -14,7 +14,7 @@ SERVICE_ACCOUNT_KEY = """
   "private_key_id": "1234567890abcdef1234567890abcdef12345678",
   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqh...\n-----END PRIVATE KEY-----\n",
   "client_email": "demo-service-account@my-demo-project.iam.gserviceaccount.com",
-  "client_id": "111111111111111111111",
+  "client_id": "33333",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
